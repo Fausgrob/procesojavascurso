20d59305affdf77e51cc32f3286a05d499d9dc0c
Fasutino Groba <faustino.groba@gmail.com>
2022-11-29 21:57:47 -0300

    comenzando con java

---
+++ clase 1/index.js
@@ -0,0 +1,30 @@
+console.log("ASÍ SE PROGRAMA DE VERDAD")
+
+const gustoFavoritoDeRaul = "Frambuesa"
+
+const nombre = "Raul"
+
+let edad = 18
+
+raul = 24
+
+console.log (edad)
+
+let edadDelUsuario = 56
+
+edadDeRaul = 12
+
+const segundoNombre = "Fernando"
+console.log(segundoNombre)
+
+console.log(10 / 2)
+
+console.log (2 * 4 + 5)
+
+alert ("hola estas en mi pagina")
+
+const edadRealDelUsuario = prompt("ingrese su edad")
+
+console.log (`Usted tiene: ${ edadRealDelUsuario }`)
+
+
